b7d6af6af05dbbcd520e130c83a2399b84d2ebde
zhangwei <1102471399@qq.com>
2024-01-14 22:55:27 +0800

    first commit niuniu

---
+++ src/main/java/com/zw/dubbopro/controller/Test.java
@@ -0,0 +1,19 @@
+package com.zw.dubbopro.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/test")
+public class Test {
+    /**
+     * 项目启动测试
+     * @return
+     */
+    @GetMapping("/startUp")
+    public String startUp() {
+
+        return "success";
+    }
+}
